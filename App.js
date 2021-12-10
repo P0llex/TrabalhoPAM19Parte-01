b4d30ff7368ff0b7efa46ad6f071dcd7446e10e2
Jonatas Costa da Silva <94843470+P0llex@users.noreply.github.com>
2021-12-09 21:54:57 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,30 @@
 import React from 'react'
 import { Text, View, StyleSheet, Button, TextInput } from 'react-native'
+import { Menu, MenuItem, MenuDivider } from 'react-native-material-menu';
 
+export default function App() {
+  const [visible, setVisible] = useState(false);
+
+  const hideMenu = () => setVisible(false);
+
+  const showMenu = () => setVisible(true);
+
+  return (
+    <View style={{ height: '100%', alignItems: 'center', justifyContent: 'center' }}>
+      <Menu
+        visible={visible}
+        anchor={<Text onPress={showMenu}>Menu</Text>}
+        onRequestClose={hideMenu}
+      >
+        <MenuItem onPress={hideMenu}>Home</MenuItem>
+        <MenuItem onPress={hideMenu}>Sobre</MenuItem>
+        <MenuItem disabled>Disabled item</MenuItem>
+        <MenuDivider />
+        <MenuItem onPress={hideMenu}>Detalhes</MenuItem>
+      </Menu>
+    </View>
+  );
+}
 
 export default class App extends React.Component {
 
